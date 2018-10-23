28337ef5f0755682d6301b46433fe0bfef6b97c3
smartbulletosx <37243514+smartbulletosx@users.noreply.github.com>
2018-10-23 07:20:02 -0700

    Create linked.cpp

---
+++ linked.cpp
@@ -0,0 +1,158 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <map>
+#include <set>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+struct Node{
+    int val;
+    Node *l, *r;
+    Node(){
+        l = nullptr;
+        r = nullptr;
+    }
+};
+
+struct LinkedList{
+    int size;
+    Node *first, *last;
+
+    LinkedList(){
+        last = new Node();
+        first = last;
+        size = 0;
+    }
+
+    void addBack(int el){
+        if(size == 0){
+            last = new Node();
+            first = last;
+            last->val = el;
+        }
+        else {
+            Node *node = new Node();
+            node->val = el;
+            last->r = node;
+            node->l = last;
+            last = node;
+        }
+        size++;
+    }
+
+    void addFront(int el){
+        if(size == 0){
+            last = new Node();
+            first = last;
+            last->val = el;
+        }
+        else {
+            Node *node = new Node();
+            node->val = el;
+            first->l = node;
+            node->r = first;
+            first = node;
+        }
+        size++;
+    }
+
+    void showList(){
+        Node *t = first;
+        while(t != nullptr){
+            cout << t->val << " ";
+            t = t->r;
+        }
+        cout << endl;
+    }
+
+    void pop_back(){
+      if(size == 0){
+            cout << 0 << endl;
+        }
+        else {
+            last->r = nullptr;
+            last->val = 1 ;
+
+        }
+        size--;
+    }
+    void begin(){
+        Node *t = first;
+        while(t->l != nullptr){
+            t = t->l;
+        }
+        cout << t->val << endl;
+    }
+
+     void end(){
+        Node *t = first;
+        while(t->r != nullptr){
+            t = t->r;
+        }
+        cout << t->val << endl;
+    }
+
+    void bol(){
+         Node *t = first;
+         int max = 0;
+        while(t != nullptr){
+            if (t->val>max) max = t->val;
+            t = t->r;
+        }
+        cout << max << endl;
+    }
+
+    void mal(){
+         Node *t = first;
+         int min = 2000000000;
+        while(t != nullptr){
+            if (t->val<min) min = t->val;
+            t = t->r;
+        }
+        cout << min << endl;
+    }
+
+    void len(){
+    cout << size<< endl;
+    }
+
+    void isEmpty(int el){
+        Node *t = first;
+        while(t != nullptr){
+            if (t->val==el) return 1;
+            t = t->r;
+        }
+        return 0;
+    }
+};
+
+int toInt(string s){
+    int ans = 0, r = 1;
+    for(int i = (int)s.size() - 1; i >= 0; i--){
+        ans += (s[i] - '0') * r;
+        r *= 10;
+    }
+    return ans;
+}
+
+int main(){
+    LinkedList list;
+     for (int i = 0; i < 30; i++) {
+        list.addFront(i+1);
+    }
+    list.showList();
+    list.pop_back();
+    list.showList();
+    list.begin();
+    list.end();
+    list.len();
+    list.bol();
+    list.mal();
+}
+
+
+
